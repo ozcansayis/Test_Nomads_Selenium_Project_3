b16425c7efa24aa2c03616cb98d31ce82e2cf9fb
umiteser <eser_umit@hotmail.com>
2024-03-17 18:24:26 +0300

    User story step 4.

--- src/TC_307.java
+++ src/TC_307.java
@@ -2,6 +2,7 @@ import Utility.BaseDriver;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class TC_307 extends BaseDriver {
     @Test
@@ -15,7 +16,9 @@ public class TC_307 extends BaseDriver {
         WebElement logo= driver.findElement(By.xpath("//*[@href='/']"));
         dAct.moveToElement(logo).click().build().perform();
 
+        //Assert.assertTrue(logo.getText().toLowerCase().equals("https://www.e-junkie.com/"));
 
+        Assert.assertFalse(driver==logo, "Hata mesajı alındı");
 
         WaitAndClose();
     }
