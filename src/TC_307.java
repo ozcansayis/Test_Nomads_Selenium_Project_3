cb363b6d36588a4819e75642525b236dc0c2e643
umiteser <eser_umit@hotmail.com>
2024-03-17 17:18:05 +0300

    User stroy stp 2.

--- src/TC_307.java
+++ src/TC_307.java
@@ -1,9 +1,19 @@
 import Utility.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class TC_307 extends BaseDriver {
     @Test
     public void TC_307() {
+
+        driver.get("https://shopdemo.e-junkie.com/");
+
+        WebElement e_junkie=driver.findElement(By.linkText("E-commerce by E-junkie"));
+        dAct.moveToElement(e_junkie).click().build().perform();
+
+        WebElement logo= driver.findElement(By.xpath("//div[@class='column is-2-desktop is-4-tablet is-4-mobile']"));
+        dAct.moveToElement(logo).click().build().perform();
 
 
 
