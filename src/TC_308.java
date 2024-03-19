0b0cffddaa70972a82f1070a82eb7da7d30e92a3
Samed <samfromda01@hotmail.com>
2024-03-19 15:06:32 +0300

    Repeated and unnecessary codes have been removed.

--- src/TC_308.java
+++ src/TC_308.java
@@ -1,61 +1,42 @@
 import Utility.BaseDriver;
-import Utility.Tools;
+import Utility.eJunkie_POM;
+import Utility.sRobot;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.awt.*;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.util.List;
+
 
 public class TC_308 extends BaseDriver {
-
     @Test
-    public void TC_308() throws AWTException {
-        Robot robocop = new Robot();
+    public void TC_308() {
+        Utility.eJunkie_POM locators = new eJunkie_POM();
         driver.get("https://www.e-junkie.com/");
-
         //wait till the ad pops up and close it clicking on a blank part of the website.
-        List<WebElement> ad = wait.until(ExpectedConditions.visibilityOfAllElements
-                (driver.findElement(By.cssSelector("div[class='wrapper']"))));//ad might not pop everytime test is run so list is added to set the test to run according to any possible scenarios.
-        for (WebElement element : ad) {
+        wait.until(ExpectedConditions.visibilityOfAllElements(locators.ad));
+        for (WebElement element : locators.ad) {                   //ad might not appear everytime test is run so list is included to set the test to run according to any possible scenarios.
             if (element.getText().contains("14-Day FREE Trial")) {
-                robocop.mouseMove(300, 300);
-                robocop.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                robocop.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                sRobot.clickBlank("click", 300, 300);
             }
         }
-        WebElement seeHowItWorks = driver.findElement(By.cssSelector("[onclick='toggleYoutubeModal(true)']"));
-        seeHowItWorks.click();
+        locators.seeHowItWorks.click();
         Assert.assertEquals("hata", "https://www.e-junkie.com/", driver.getCurrentUrl());
 
         //reaching the play button utilizing shift function of robot class and entering it.
-        for (int i = 0; i < 15; i++) {
-            Tools.WaitDouble(1);
-            robocop.keyPress(KeyEvent.VK_TAB);
-            robocop.keyRelease(KeyEvent.VK_TAB);
-        }
-        robocop.keyPress(KeyEvent.VK_ENTER);
-        robocop.keyRelease(KeyEvent.VK_ENTER);
-        //muted the vid.
-        robocop.keyPress(KeyEvent.VK_M);
-        robocop.keyRelease(KeyEvent.VK_M);
+        sRobot.robots("tab", 15);
+        sRobot.robots("enter", 1);
 
-        WebElement frame = driver.findElement(By.cssSelector("div[class='modal youtube-modal is-active'] iframe"));
-        driver.switchTo().frame(frame);
-
+        driver.switchTo().frame(locators.frame);
         //if the info video reachs 10th second we hit the stop button
-        WebElement timeDisp = driver.findElement(By.cssSelector("div[class='ytp-time-display notranslate'] span span:nth-child(1)"));
         while (true) {
-            String time = timeDisp.getText();
-            if (time.equals("0:10")) {
-                robocop.keyPress(KeyEvent.VK_ENTER);
-                robocop.keyRelease(KeyEvent.VK_ENTER);
+            wait.until(ExpectedConditions.visibilityOf(locators.timeDisp));
+            int time = Integer.parseInt(locators.timeDisp.getText().substring(2));
+            if (time > 9) {
+                driver.quit();
                 break;
+            } else {
+                sRobot.clickBlank("dontClick", 1250, 750);
             }
         }
-        WaitAndClose();
     }
 }
