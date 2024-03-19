ffafebb40094138ef01bdd1fa15b8fbf61c3fb9e
ozcansayis <ozcansayis@gmail.com>
2024-03-19 23:13:33 +0300

    hotfix

--- src/Utility/sRobot.java
+++ src/Utility/sRobot.java
@@ -3,8 +3,7 @@ package Utility;
 import org.junit.Assert;
 
 import java.awt.*;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class sRobot {
     public static Robot robot;
@@ -28,19 +27,16 @@ public class sRobot {
             System.out.println("Invalid Action");
             Assert.fail();
         }
-
     }
 
-
-
     private static void tabPresser(int time) {
-
         for (int i = 0; i < time; i++) {
-            Tools.WaitDouble(1);
+            Tools.WaitNano(1);
             robot.keyPress(KeyEvent.VK_TAB);
             robot.keyRelease(KeyEvent.VK_TAB);
         }
     }
+
     public static void clickBlank(String click, int x, int y) {
         if (click.equalsIgnoreCase("click")) {
             robot.mouseMove(x, y);
@@ -50,23 +46,22 @@ public class sRobot {
             robot.mouseMove(x, y);
         }
     }
-    private static void tabPresserReverse(int time) {
 
+    private static void tabPresserReverse(int time) {
         for (int i = 0; i < time; i++) {
-            Tools.WaitDouble(1);
+            Tools.WaitNano(1);
             robot.keyPress(KeyEvent.VK_SHIFT);
             robot.keyPress(KeyEvent.VK_TAB);
             robot.keyRelease(KeyEvent.VK_TAB);
             robot.keyRelease(KeyEvent.VK_SHIFT);
         }
     }
+
     private static void enterPresser(int time) {
         for (int i = 0; i < time; i++) {
-            Tools.WaitDouble(1);
+            Tools.WaitNano(1);
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
         }
-
     }
-
 }
