2508f22d70fd2c0866bd467c70bae2b4843aa43e
ozcansayis <ozcansayis@gmail.com>
2024-03-19 23:10:23 +0300

    Empty lines deleted.

--- src/TC_308.java
+++ src/TC_308.java
@@ -6,15 +6,15 @@ import org.junit.Test;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-
 public class TC_308 extends BaseDriver {
     @Test
     public void TC_308() {
         Utility.eJunkie_POM locators = new eJunkie_POM();
         driver.get("https://www.e-junkie.com/");
+
         //wait till the ad pops up and close it clicking on a blank part of the website.
         wait.until(ExpectedConditions.visibilityOfAllElements(locators.ad));
-        for (WebElement element : locators.ad) {                   //ad might not appear everytime test is run so list is included to set the test to run according to any possible scenarios.
+        for (WebElement element : locators.ad) { //ad might not appear everytime test is run so list is included to set the test to run according to any possible scenarios.
             if (element.getText().contains("14-Day FREE Trial")) {
                 sRobot.clickBlank("click", 300, 300);
             }
@@ -27,6 +27,7 @@ public class TC_308 extends BaseDriver {
         sRobot.robots("enter", 1);
 
         driver.switchTo().frame(locators.frame);
+
         //if the info video reachs 10th second we hit the stop button
         while (true) {
             wait.until(ExpectedConditions.visibilityOf(locators.timeDisp));
